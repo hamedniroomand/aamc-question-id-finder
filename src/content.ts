46accef07543fbc0393fefbeacd5d8a5b8501e92
Hamed Niroomand <hamed@niroomand.dev>
2025-02-16 19:39:12 +0330

    refactor: simplify component removal and improve code organization

--- src/content.ts
+++ src/content.ts
@@ -1,15 +1,16 @@
 import { mount } from "svelte";
 import QuestionWindow from "./lib/QuestionWindow.svelte";
 
-const removeBadge = () => {
-    const wrapper = document.getElementById('aamc-question-id-finder');
+const wrapperId = 'aamc-question-id-finder';
+
+const removeComponent = () => {
+    const wrapper = document.getElementById(wrapperId);
     if (wrapper) {
         wrapper.remove();
     }
 }
 
 const initialWrapper = () => {
-    const wrapperId = 'aamc-question-id-finder';
     if (document.getElementById(wrapperId)) {
         document.body.removeChild(document.getElementById(wrapperId)!);
     }
@@ -24,6 +25,6 @@ const target = initialWrapper();
 mount(QuestionWindow, {
     target,
     props: {
-        removeComponent: () => removeBadge(),
+        removeComponent,
     }
 });
